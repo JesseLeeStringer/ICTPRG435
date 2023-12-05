aa66dbb93914af1e172dabdf3d90b2e034ee0925
JesseLeeStringer <jesseleestringer@gmail.com>
2023-12-05 17:56:44 +1100

    Added feature: colorama to notification areas

--- User Interface.py
+++ User Interface.py
@@ -5,13 +5,13 @@ Unit: ICTPRG435
 Development Date: 03/12/2023
 Source: https://github.com/JesseLeeStringer/ICTPRG435
 """
-import os.path
-import sys
-import time
-from colorama import Fore, Back, Style
+import time  # Need it for "Sleep"
+from colorama import Fore, Back, Style  # Let's create notifications with Colour, and Reset Styles
+import art  # Art work for Option #3/4
+from art import *  # Let's import all the art works
 
 
-def menu():
+def menu():  # Give the user an option of four choices.
     print("1. Input Credentials")
     print("2. View Passwords")
     print("3. View Credit-Card")
@@ -34,8 +34,8 @@ def menu():
         if input_password != input_password_confirm:
             exit("Passwords do not match!")
         else:
-            print(Fore.RED + "Password Confirmed" + "\n")
-            print(Style.RESET_ALL + "Commence to Credit-Card")
+            print(Fore.RED + "Password Confirmed" + "\n")  # Give User feedback on progress
+            print(Style.RESET_ALL + "Commence to Credit-Card")  # Reset Styling & Progression
 
         # https://stackoverflow.com/questions/48817461/user-input-boolean-in-python
         user_cc_info_answer = input('Do you need to save a Credit-Card? (Y/N) : ').lower().strip()
@@ -66,7 +66,8 @@ def menu():
                 print(Fore.RED + input_url, "|", input_user, "|", "XXXX XXXX XXXX", user_cc_display[-4:])
                 print("url | user | Credit Card Ending")
                 print(Style.RESET_ALL + "########################################")
-                exit("Information Saved to file ")
+                menu()
+
             else:
                 time.sleep(0.2)  # Wait for 3 Seconds #https://ioflood.com/blog/python-wait/
                 print(Fore.RED + "*** Error! 16 characters are required! ***")
@@ -94,22 +95,22 @@ def menu():
             print(Style.RESET_ALL + "########################################" + "\n")
             menu()
 
-    elif menu_selection == "2":
-        print("Opening Passwords" + "\n")
-        print("domain | user | pass | Credit-Card")
-        view_passwords = open('passwords.txt', 'r')
-        passwords = view_passwords.read()
-        print(passwords)
-        print("\n")
+    elif menu_selection == "2":  # Open the Password.txt file.
+        print("Opening passwords.txt" + "\n")  # Give a notification to the user.
+        print(Fore.RED + "DOMAIN | USER | PASS | CREDIT CARD" + Style.RESET_ALL)  # Make it Red and Bloody Obvious
+        view_passwords = open('passwords.txt', 'r')  # Open the file in 'r'ead mode.
+        passwords = view_passwords.read()  # Show the contents of the password.txt file as "passwords"
+        print(passwords + "\n")  # Print/Show the contents + new line formatting.
         menu()
 
-    elif menu_selection == "3":
-        print(Fore.BLUE + "*** WARNING *** PCI-DSS Systems Daemon Activated")
-        time.sleep(1)
+    elif menu_selection == "3":  # Scare Zahir
+        print(
+            Fore.BLUE + "*** WARNING *** PCI-DSS Systems Daemon Activated")  # Change Colours, & Warning to show progression
+        time.sleep(1)  # Sleep it, see if the user freaks out.
         print(Fore.BLUE + "*|* WARNING *|* PCI-DSS Systems Loading")
-        time.sleep(1)
+        time.sleep(1)  # Sleep it again, make it looks realistic
         print(Fore.BLUE + "*|| WARNING *|| PCI-DSS Systems Loading")
-        time.sleep(1)
+        time.sleep(1)  # Sleep once more....
         print(Fore.BLUE + "||| WARNING ||| PCI-DSS Systems Loading")
         time.sleep(0.5)
         print(Fore.YELLOW + "PCI-DSS Systems Loading: 33%.")
@@ -122,9 +123,9 @@ def menu():
         time.sleep(2.4)
         print(Fore.RED + "PCI-DSS Systems LOADED...")
         time.sleep(1)
-        print("Confirm Destroying System32 Files? :")
-        let_them_suffer = input("Y/N :")
-        if let_them_suffer:
+        print("Confirm Destroying System32 Files?")
+        let_them_suffer = input("Y/N :")  # Give the User a "Choice"...
+        if let_them_suffer:  # Let's give the user a dud prompt to fail at.
             print("del c:/Windows/System32/*.*")
             print("X/X")
             time.sleep(1)
@@ -137,12 +138,19 @@ def menu():
             print("(☞ﾟヮﾟ)☞ ┬─┬")
             time.sleep(2)
             print("(╯°□°)╯︵ ┻━┻")
-            time.sleep(5)
+            time.sleep(2)
+            print(text2art('''BOOM!!!
+                    ''', font="small"))  # Make it fun.
+
             exit("Systems Offline")
+
         else:
             menu()
     else:
-        print("Unacceptable!" + "\n")
+        print(text2art('''UNACCEPTABLE 
+        TRY 
+        AGAIN!!!
+        ''', font="small"))  # Make it fun.
         menu()
 
 
